b03587efa02b0eaf7093eec1fe7dbb53b302d792
Monti <111462978+monti212@users.noreply.github.com>
2026-02-26 19:17:21 +0200

    Deleted kb-api.ts

--- src/rag/index.cjs
+++
@@ -1,6 +0,0 @@
-const { indexDocuments } = require('./rag.cjs');
-const path = require('path');
-
-const DOCS_DIR = path.join(__dirname, '../../');
-indexDocuments(DOCS_DIR);
-console.log('Indexed documents in', DOCS_DIR);
